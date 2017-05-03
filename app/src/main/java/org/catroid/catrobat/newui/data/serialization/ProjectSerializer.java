f1e3038bbdb4500c0ecff753cbf4f2ca07be26ac
Matthias Link <matthias@link.co.at>
2017-05-03 11:19:31 +0200

    WIP

---
+++ app/src/main/java/org/catroid/catrobat/newui/data/serialization/ProjectSerializer.java
@@ -0,0 +1,9 @@
+package org.catroid.catrobat.newui.data.serialization;
+
+import org.catroid.catrobat.newui.data.ProjectItem;
+
+public class ProjectSerializer {
+    public String serializeProject(ProjectItem project) {
+
+    }
+}
